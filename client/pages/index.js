5b669f0eafa1ddac5483f9d4b33cad792f84e2ef
Sebastian Burman <sebastianjburman@gmail.com>
2022-09-17 21:52:45 -0400

    Added example tests

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1>PerpetualMotion</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
