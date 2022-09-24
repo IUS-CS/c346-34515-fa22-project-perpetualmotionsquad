e4c9084e6b536dbb9ee74054a66c17ceaf2f112b
Robinsbw <Robinsbw1217@gmail.com>
2022-09-24 16:02:03 -0400

    set up the initial design layout for the home page

---
+++ client/pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function About () {
+    return (
+        <ul>
+            <h1>Place holder for information about the project</h1>
+            <Link href="/">Home Link</Link>
+        </ul>
+    )
+}
---
+++ client/pages/searchResults.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function searchResults () {
+    return (
+        <ul>
+            <h1>Place holder for the search results page</h1>
+            <Link href="/">Home Link</Link>
+        </ul>
+    )
+}
