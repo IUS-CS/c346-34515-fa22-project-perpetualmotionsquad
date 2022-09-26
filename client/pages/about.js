20245820fd4005acdeab877194a9b6c2059d09ad
Robinsbw <Robinsbw1217@gmail.com>
2022-09-25 22:41:18 -0400

    added image to head, added link to about page to the homepage, set up blank about page

---
+++ client/pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function about () {
+    return (
+        <main>
+            <h1>Place holder for information about the project</h1>
+            <Link href="/">Home Page</Link>
+        </main>
+    )
+}
