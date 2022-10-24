ac09d3d8976896e9ae0cad189151f80779e15ea9
Sebastian Burman <sebastianburman@pop-os.localdomain>
2022-10-23 20:52:10 -0400

    Wrote test to test  homepage title and homepage image renders with correct content

---
+++ client/__tests__/indexpage/index.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Home from "../../pages/index"
+
+test('Home page title contains correct title', () => {
+  render(<Home />);
+  const element = screen.getByTestId('homeTitle')
+  console.log(element.textContent)
+  expect(element.textContent).toEqual("DestiNation");
+});
+
+test('Home page image src and alt contains correct value', () => {
+  render(<Home />);
+  const element = screen.getByTestId('homeImage')
+  expect(element.src).toContain("airplane.png");
+  expect(element.alt).toEqual("Airplane Image");
+});
